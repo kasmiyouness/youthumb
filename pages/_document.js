bdf31667f1c051b5413efabd4acbc26ad47239bc
Miryhub <miri_youssef@hotmail.com>
2023-09-04 13:42:20 +0100

    adding adsense

--- pages/_document.js
+++ pages/_document.js
@@ -23,6 +23,7 @@ class MyDocument extends Document {
             href="/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         </Head>
         <body>
           <Main />
