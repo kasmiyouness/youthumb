e48a267c23d09a53089bc98a41857224942f8649
azersoufiane <usesouf@gmail.com>
2023-12-10 06:59:02 +0100

    texts seo

--- pages/_document.js
+++ pages/_document.js
@@ -30,6 +30,23 @@ class MyDocument extends Document {
         </Head>
         <body>
           <Main />
+          
+          {/* Additional text for the YouTube Thumbnail Downloader */}
+          <div>
+            <h1>YouTube Thumbnail Downloader</h1>
+            <p>
+              ▷ Save YT Video Thumbnails
+              Get free thumbnail image of any youtube videos in Full HD(1080), HD (720), SD, and also in small size.
+              It's currently supported formats: YouTube (HD, HQ, 1080p, 4K) videos
+            </p>
+            <p>
+              What is the use of this YouTube Thumbnail Grabber website?
+              People use this YouTube thumbnail downloader website for getting thumbnail from any youtube videos.
+              That can be used in presentation, animation work or many other activities.
+            </p>
+            {/* Add more paragraphs as needed */}
+          </div>
+
           <NextScript />
         </body>
       </Html>
