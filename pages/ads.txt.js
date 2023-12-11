6933f3bcfa02a58ce79c349f8792e32aab524076
azersoufiane <usesouf@gmail.com>
2023-12-11 20:38:43 +0100

    ads.txt

---
+++ pages/ads.txt.js
@@ -0,0 +1,19 @@
+// pages/ads.txt.js
+const AdsTxt = () => null;
+
+export const getServerSideProps = ({ res }) => {
+  // Your actual ads.txt content
+  const adsTxtContent = `
+  google.com, pub-1932386256787608, DIRECT, f08c47fec0942fa0
+    // Add more lines as needed
+  `;
+
+  // Set response headers
+  res.setHeader('Content-Type', 'text/plain');
+  res.status(200).send(adsTxtContent);
+
+  // Return an empty object to prevent Next.js from trying to render a React component
+  return { props: {} };
+};
+
+export default AdsTxt;
